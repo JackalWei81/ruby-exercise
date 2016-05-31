17023aa8c1b9f9cc12f2f24e8bac30ec8c5518ea
jackalwei <jackal8125@gmail.com>
2016-05-31 14:38:15 +0800

    basic/read7

---
+++ basic-ex7.rb
@@ -0,0 +1,5 @@
+#read7
+#SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+#from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+#表示缺少closing bracket，需再檢查程式碼哪邊少了closing bracket
