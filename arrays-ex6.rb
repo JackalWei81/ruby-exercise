71720aed5be26f336b5230114e8ac1defc07c159
jackalwei <jackal8125@gmail.com>
2016-05-31 15:57:13 +0800

    arrays/ex6

---
+++ arrays-ex6.rb
@@ -0,0 +1,10 @@
+=begin
+TypeError: no implicit conversion of String into Integer
+from (irb):2:in `[]='
+from (irb):2
+from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+=end
+
+=begin
+  array的index為數字，不可以使用字串，這是定理＠＠
+=end
