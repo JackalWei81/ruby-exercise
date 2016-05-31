3413291fbc3acc05bb2f02c3fdf16e83a1119d40
jackalwei <jackal8125@gmail.com>
2016-05-31 15:08:07 +0800

    method/read6

---
+++ method-ex6.rb
@@ -0,0 +1,8 @@
+=begin
+ArgumentError: wrong number of arguments (1 for 2)
+from (irb):1:in `calculate_product'
+from (irb):4
+from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+=end
+
+#以上錯誤表示傳入method的參數數目錯誤，檢查呼叫calculate_product這個method的參數數目
